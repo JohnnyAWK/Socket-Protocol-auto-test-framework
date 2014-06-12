3fb41454f3f8bd92ed3124b614a40bb82d31015b
JohnnyAWK <289008087@qq.com>
2014-06-12 11:46:53 +0800

    Update IPackageContenParser.java

--- src/com/dbStudio/gameTest/packageContentParser/IPackageContenParser.java
+++ src/com/dbStudio/gameTest/packageContentParser/IPackageContenParser.java
@@ -1,15 +1,6 @@
 package com.dbStudio.gameTest.packageContentParser;
 
-/**
- *<br> 协议 = 协议头 + 协议体</br>
- * <br>一般协议头为2或4字节，用以表示协议体的长度，</br>
- * 
- * <br>女妖的发送协议包为：协议头(L) + 协议体(cmd、data)</br>
- * <br>女妖的接收协议包为：协议头(L) + 协议体(cmd、Zip、data)</br>
- * 
- *<br> 七雄的发送包和接收包格式一致： 协议头(4字节表示协议体长度) + 协议体(1字节是否压缩标志位、data)</br>
- * 
- * */
+
 public interface IPackageContenParser {
 	
 	public int getProtocolHeadLength();
